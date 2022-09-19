c0cf137dba0a75c4cffd6353b93b25dafdf3a99a
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-19 14:40:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT instructor_id, instructor_name from instructor";
+$sql = "SELECT instructor_id, instructor_name from Instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
