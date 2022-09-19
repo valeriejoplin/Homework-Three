451b5b2b69c9813dd366a44118987acdea6166f7
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-19 14:11:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+<?php
+require_once("header.php");
+?>
   <body>
  <table class ="table table-striped>
   <thead>
@@ -28,7 +23,23 @@ if ($conn->connect_error) {
                 
 $sql = "SELECT instructor_id, instructor_name from instructor";
 $result = $conn->query($sql);
-                
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["instructor_id"]?></td>
+    <td><?=$row["instructor_name"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>           
 
             
 
