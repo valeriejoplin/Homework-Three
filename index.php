a61284712bf9af87f99e7d844b541610d10ebdc8
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-19 14:13:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -40,14 +40,6 @@ $conn->close();
 ?>
   </tbody>
     </table>           
-    <h1>Hello, world!</h1>
-    <form action="handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-    
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
