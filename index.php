c914d73caf3102499d7ec64c0242dfb2903063c7
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-21 13:42:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if ($result->num_rows > 0) {
 ?>
   <tr>
     <td><?=$row["instructor_id"]?></td>
-    <td><?=$row["instructor_name"]?></td>
+    <td>a href="instructor-section.php?id=<?=$row["instructor_id"]?>"><?=$row["instructor_name"]?></a></td>
   </tr>
 <?php
   }
