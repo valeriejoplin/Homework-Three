8cf4790aafdc01b3c8bbd994a2b8d27f7e3284e9
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-21 14:14:49 -0500

    Update student.php

--- student.php
+++ student.php
@@ -1,3 +1,51 @@
 <?php
 require_once("header.php");
 ?>
+ <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>StudentID</th>
+      <th>Name</th>
+      <th>Major</th>
+      <th>Graduation Date</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+$servername = "localhost";
+$username = "valeriej_databaseuser";
+$password = "tI_*dXAL^r[(";
+$dbname = "valeriej_homework3";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT StudentID, Name, Major, GradYear from Student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["StudentID"]?></td>  
+    <td><?=$row["Name"]?></td>  
+    <td><?=$row["Major"]?></td>
+    <td><?=$row["GradYear"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+<?php
+require_once("footer.php");
+?>
