40a8aa84109b12759502e9c405fb632567a32be2
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-21 13:59:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT instructor_id, instructor_name from Instructor";
+$sql = "SELECT Instructor_ID, Instructor_Name from Instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -30,8 +30,8 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["instructor_id"]?></td>
-    <td><a href="instructor-section.php?id=<?=$row["instructor_id"]?>"><?=$row["instructor_name"]?></a></td>
+    <td><?=$row["Instructor_id"]?></td>
+    <td><a href="instructor-section.php?id=<?=$row["Instructor_ID"]?>"><?=$row["Instructor_Name"]?></a></td>
   </tr>
 <?php
   }
