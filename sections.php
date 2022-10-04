c82a91dc4a918e6321480e56a755dd27c9dced7c
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-04 15:04:51 -0500

    Update sections.php

--- sections.php
+++ sections.php
@@ -1,3 +1,57 @@
 <?php
 require_once("header.php");
 ?>
+<body>
+    <h1>Sections</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Prefix</th>
+      <th>Number</th>
+      <th>Section</th>
+      <th>Instructor</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "valeriej_databaseuser";
+$password = "tI_*dXAL^r[(";
+$dbname = "valeriej_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select Section_ID, Section_Number, i.Instructor_Name, c.Prefix, c.Number from Section s join Instructor i on i.Instructor_ID = s.Instructor_ID join Course c on c.CourseID = s.CourseID";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["Section_ID"]?></td>
+    <td><?=$row["Prefix"]?></td>
+    <td><?=$row["Number"]?></td>
+    <td><?=$row["Section_Number"]?></td>
+    <td><?=$row["Instructor_Name"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    
+ <?php
+    require_once("footer.php");
+  ?>
+    
