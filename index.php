5260ef545d523849b525968df3acd39cda0fc5e5
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 12:40:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require_once("header.php")
 ?>
 <body style = "background-color:crimson;text-align:center";>
 <h2> Welcome to the University Database </h2>
-<h6> Use the Navigation bar above to look around the website </h6>
+<h6> Use the Navigation bar above to look around the website! </h6>
+
 </body>
 <?php 
 require_once("footer.php")
