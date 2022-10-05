c1ec25e01e251f7a65f888b9e766b2912be50479
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-04 22:28:27 -0500

    Update header.php

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
           <a class="nav-link" href="student.php">Students</a>
         </li>
       <li class="nav-item">
-          <a class="nav-link" href="section.php">Sections</a>
+          <a class="nav-link" href="sections.php">Sections</a>
         </li>
       </ul>
     </div>
