bf2d2de0667f1b2bf738347aff9fa137f3008115
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 11:16:25 -0500

    Update sections.php

--- sections.php
+++ sections.php
@@ -28,7 +28,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select SectionID, Section_Number, Instructor_Name, c.Prefix, c.Number from Sections s join Instructor i on i.InstructorID = s.InstructorID join Course c on c.CourseID = s.CourseID";
+$coid = $_POST['id'];
+$sql = "select SectionID, Section_Number, Instructor_Name, c.Prefix, c.Number from Sections s join Instructor i on i.InstructorID = s.InstructorID join Course c on c.CourseID = s.CourseID=" . $coid;
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
