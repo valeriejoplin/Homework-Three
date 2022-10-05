23f9513b511eb45ad442de2a4f050d74c74ea5ab
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-05 12:36:51 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+require_once("header.php")
+?>
+<body style = "background-color:crimson";>
+<h2> Welcome to the University Database </h2>
+<h6> Use the Navigation bar above to look around the website </h6>
+</body>
+<?php 
+require_once("footer.php")
+?>
