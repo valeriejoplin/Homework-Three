73f1a4cbe96964824c5109f6517213fa30d646a7
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-19 13:52:28 -0500

    Update instructor-section.php

--- instructor-section.php
+++ instructor-section.php
@@ -26,7 +26,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
   die("Connection failed: " . $conn->connect_error);
 }
 $iid = $_GET['id'];
-$sql = "select SectionID, Section_Number, i.Name, c.Prefix, c.Number from Sections s join Instructor i on i.InstructorID= s.InstructorID join Course c on c.CourseID=s.CourseID where i.Instructor=" . $iid;
+$sql = "Select * from Instructor Where InstructorID=" . $iid;
     $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
