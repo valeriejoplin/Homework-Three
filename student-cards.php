bab303aa8eb610d2ae341c1c82354a6593d57c44
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-10-20 11:24:15 -0500

    Update student-cards.php

--- student-cards.php
+++ student-cards.php
@@ -14,7 +14,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$iid = $_GET['id'];
+$iid = $_POST['id'];
 $sql = "SELECT * from Student WHERE StudentID=" . $iid;
 $result = $conn->query($sql);
 
